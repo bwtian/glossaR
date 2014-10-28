e588dc94eb1c8970c49efbb0d7c9972dfeb0c987
Bingwei Tian <bwtian@gmail.com>
2014-10-28 15:21:20 +0900

    / ‘R/glsRestec.R’

--- R/glsRestec.R
+++ R/glsRestec.R
@@ -1,5 +1,5 @@
 glsXML  <- function(url, short = "//dt", long = "//dd"){
-        require(XML)
+        require(XML, stringr)
         doc  <- htmlParse(url)
         short.l  <- XML::xpathApply(doc, short)
         s  <- sapply(short.l, function(x) {
@@ -19,17 +19,16 @@ glsXML  <- function(url, short = "//dt", long = "//dd"){
 }
 url  <- c("http://www.restec.or.jp/english/knowledge/glossary.html")
 x  <- glsXML(url)
-pre  <- "http://www.restec.or.jp/english/knowledge/glossary"
-suf  <- c("","-bc","-de","-fh","-ik","-ln","-oq","-rs","-tu","-vz","-09")
-url.l  <- paste0(pre, suf, ".html")
+# pre  <- "http://www.restec.or.jp/english/knowledge/glossary"
+# suf  <- c("","-bc","-de","-fh","-ik","-ln","-oq","-rs","-tu","-vz","-09")
+# url.l  <- paste0(pre, suf, ".html")
+#
+# df.l  <- lapply(url.l, glsXML)
+# df.d  <- as.data.frame(do.call(rbind, df.l))
+#
+# org  <- paste0("** ", df.d[,1],":", df.d[,2])
+# org
+# write.table(org,
+#             "test.org", sep = "\n", append = T,quote = F,
+#             row.names = F, col.names = F)
 
-df.l  <- lapply(url.l, glsXML)
-df.d  <- as.data.frame(do.call(rbind, df.l))
-
-org  <- paste0("** ", df.d[,1],":", df.d[,2])
-org
-write.table(org,
-            "test.org", sep = "\n", append = T,quote = F,
-            row.names = F, col.names = F)
-getwd()
-library(stringr)
